c71875bf9c285653db8814afc93a360fa635859d
kimsyd <kimsyd@hawaii.edu>
2023-10-13 17:35:03 -1000

    meteor hello world

---
+++ app/imports/ui/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Hello } from './Hello.jsx';
+import { Info } from './Info.jsx';
+
+export const App = () => (
+  <div>
+    <h1>Testing</h1>
+    <Hello/>
+    <Info/>
+  </div>
+);
